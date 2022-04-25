6ccc574998145987395bf91f8789c82212854c24
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-25 00:58:12 -0500

    5.- Agregando una nueva ruta

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const port = 3000// Localhost: 3000
 app.get('/', (req, res) => {
     res.send("Hello World explores")
 })
+//Respondiendo texto
+//localhost:3000/launchx
+app.get('/launchx', (req, res) =>{
+    res.send('Bienvenidos a launchX')
+})
 
+// Respondiendo texto
 //Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
